c99fcf68a2d1d9ce96244a8c718f84f6d2e7f7b0
DavidHH01 <108719532+DavidHH01@users.noreply.github.com>
2025-06-29 21:22:11 +0200

    Add script setup for Google Ads integration in app.vue

--- app.vue
+++ app.vue
@@ -3,3 +3,14 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+<script setup>
+useHead({
+  script: [
+    {
+      src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7488788676367729",
+      async: true,
+      crossorigin: "anonymous",
+    },
+  ],
+});
+</script>
